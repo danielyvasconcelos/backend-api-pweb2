b85669488600aef20b9e6501aaab65d436795e31
danielyvasconcelos <desv1@aluno.ifal.edu.br>
2022-09-20 16:28:29 -0300

    continuação da aula 03

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const express = require ('express');
+
+const app = express();
+app.use(express.json());
+app.get('/teste',(req, res) => {
+res.send('bom dia'); // vai enviar os dados para a tela
+} ) //configurando uma rota, não é a melhor pratica
+
+app.listen(3000, ()=> {
+    console.log('server started');
+})
